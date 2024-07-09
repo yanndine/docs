3ce20cf097f50284a7a1e1e06e06218ee13ed6f3
obaidmuneer <obaidmuneer55@gmail.com>
2024-07-09 12:04:43 +0500

    updated script

--- start-mintlify.js.js
+++ start-mintlify.js.js
@@ -1,12 +1,43 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
+import process from "process";
+import http from "http";
 
-const command = 'mintlify dev';
+const command = 'mintlify dev --3000';
+
+let serverRunning = true;
+
+const checkServer = () => {
+    http.get('http://localhost:3000', (res) => {
+        if (res.statusCode !== 200) {
+            console.error(`Server responded with status code: ${res.statusCode}`);
+            serverRunning = false;
+        } else {
+            serverRunning = true;
+        }
+    }).on('error', (err) => {
+        console.error(`Error: ${err.message}`);
+        serverRunning = false;
+    });
+};
 
 exec(command, (err, stdout, stderr) => {
     if (err) {
         console.error(`Error executing command: ${err}`);
-        return;
+        process.exit(1);
     }
     console.log(stdout);
     console.error(stderr);
+});
+
+const interval = setInterval(() => {
+    checkServer();
+    if (!serverRunning) {
+        console.log("Server is no longer running on port 3000, exiting...");
+        clearInterval(interval);
+        process.exit(1);
+    }
+}, (60000));
+
+process.on('exit', () => {
+    clearInterval(interval);
 });
